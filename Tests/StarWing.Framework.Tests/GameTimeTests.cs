57d6aabe985d2a11b3e07311bbf7919aa4ec4666
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-04-11 17:30:57 +0300

    Add some tests for GameTime class

---
+++ Tests/StarWing.Framework.Tests/GameTimeTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using StarWing.Framework;
+
+namespace Tests.StarWing.Framework.Tests
+{
+    [TestFixture]
+    public class GameTimeTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void CanMeasureTime()
+        {
+            var gameTime = new GameTime();
+            gameTime.Start();
+            var sleepTimeout = TimeSpan.FromMilliseconds(10);
+            Thread.Sleep(sleepTimeout);
+            var actualTimeout = gameTime.TotalTime;
+            Assert.That(actualTimeout, Is.GreaterThanOrEqualTo(sleepTimeout));
+        }
+
+        [Test]
+        public void CanGetTimeSinceLastUpdate()
+        {
+            var gameTime = new GameTime();
+            gameTime.Start();
+            var sleepTimeout = TimeSpan.FromMilliseconds(10);
+            Thread.Sleep(sleepTimeout);
+            var actualTimeout = gameTime.SinceLastUpdate;
+            Assert.That(actualTimeout, Is.GreaterThanOrEqualTo(sleepTimeout));
+        }
+
+        [Test]
+        public void TimeSinceLastUpdate_ResetsAfterUpdateMethodCall()
+        {
+            var gameTime = new GameTime();
+            gameTime.Start();
+            var sleepTimeout = TimeSpan.FromMilliseconds(10);
+            Thread.Sleep(sleepTimeout);
+            gameTime.Update();
+            var actualTimeout = gameTime.SinceLastUpdate;
+            Assert.That(actualTimeout, Is.LessThanOrEqualTo(sleepTimeout));
+        }
+
+        [Test]
+        public void TimespanIsEqualToZeroBeforeStart()
+        {
+            var gameTime = new GameTime();
+            Assert.That(gameTime.SinceLastUpdate, Is.EqualTo(TimeSpan.Zero));
+            Assert.That(gameTime.TotalTime, Is.EqualTo(TimeSpan.Zero));
+        }
+    }
+}
