73e5d37af789a086e38261dcd1c442b682d0f452
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-04-11 19:57:13 +0300

    Added constructor args checking

--- StarWing/Primitives/Texture2D.cs
+++ StarWing/Primitives/Texture2D.cs
@@ -5,13 +5,15 @@ namespace StarWing.Framework.Primitives
 {
     public class Texture2D
     {
-        public Texture2D(Image sprite, float scale)
+        public Texture2D(Image image, float scale)
         {
-            Sprite = sprite;
-            Scale = scale;
+            Image = image ?? throw new ArgumentNullException(nameof(image));
+            Scale = scale > 0
+                        ? scale
+                        : throw new ArgumentOutOfRangeException(nameof(scale), "Scale can not be negative");
         }
 
         public float Scale { get; private set; }
-        public Image Sprite { get; private set; }
+        public Image Image { get; private set; }
     }
 }
