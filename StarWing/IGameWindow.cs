713b039cf47fcbf44b12e88488a6a08fd207d7c7
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-04-12 19:58:14 +0300

    Interface for game window

---
+++ StarWing/IGameWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace StarWing.Framework
+{
+    public interface IGameWindow
+    {
+        Point Position { get; }
+        int Width { get; }
+        int Height { get; }
+        string Title { get; }
+    }
+}
