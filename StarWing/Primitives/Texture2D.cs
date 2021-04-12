2426888b9aa4def973b810e8f63c22b22c71ec4d
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-04-12 20:22:20 +0300

    Add width and height public properties

--- StarWing/Primitives/Texture2D.cs
+++ StarWing/Primitives/Texture2D.cs
@@ -5,15 +5,23 @@ namespace StarWing.Framework.Primitives
 {
     public class Texture2D
     {
-        public Texture2D(Image image, float scale)
+        public Texture2D(Image image, float scale, int width, int height)
         {
             Image = image ?? throw new ArgumentNullException(nameof(image));
-            Scale = scale > 0
+            Scale = scale >= 0
                         ? scale
                         : throw new ArgumentOutOfRangeException(nameof(scale), "Scale can not be negative");
+            Width = width >= 0
+                        ? width
+                        : throw new ArgumentOutOfRangeException(nameof(width), "Width can not be negative");
+            Height = height >= 0
+                         ? height
+                         : throw new ArgumentOutOfRangeException(nameof(height), "Height can not be negative");
         }
 
-        public float Scale { get; private set; }
-        public Image Image { get; private set; }
+        public int Width { get; }
+        public int Height { get; }
+        public float Scale { get; }
+        public Image Image { get; }
     }
 }
