9792f79a1edffd4a73e1e855d0eaa3ebd9c0f978
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-04-13 12:12:01 +0300

    Added new tests

---
+++ Tests/StarWing.Framework.Input.Tests/FakeKeyboardManipulator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using StarWing.Framework.Input;
+
+namespace Tests.StarWing.Framework.Input.Tests
+{
+    public class FakeKeyboardManipulator : IKeyboardManipulator
+    {
+        private readonly List<Keys> _pressed = new();
+
+        public IReadOnlyList<Keys> Pressed =>
+            _pressed;
+
+        public void Press(Keys key)
+        {
+            _pressed.Add(key);
+            KeyDown?.Invoke(this, new KeyEventArgs(key));
+        }
+
+        public void Release(Keys key)
+        {
+            _pressed.Remove(key);
+            KeyUp?.Invoke(this, new KeyEventArgs(key));
+        }
+
+        public void ReleaseAll()
+        {
+            _pressed.ForEach(key => KeyUp?.Invoke(this, new KeyEventArgs(key)));
+            _pressed.Clear();
+        }
+
+        public event EventHandler<KeyEventArgs> KeyDown;
+        public event EventHandler<KeyEventArgs> KeyUp;
+    }
+}
---
+++ Tests/StarWing.Framework.Input.Tests/KeyboardTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Forms;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Api;
+using StarWing.Framework.Input;
+
+
+namespace Tests.StarWing.Framework.Input.Tests
+{
+    [TestFixture]
+    public class KeyboardTests
+    {
+        private FakeKeyboardManipulator _keyboardManipulator;
+        private Keyboard _keyboard;
+
+
+        public KeyboardTests()
+        {
+            _keyboardManipulator = new FakeKeyboardManipulator();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _keyboard = new Keyboard(_keyboardManipulator);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _keyboardManipulator.ReleaseAll();
+            _keyboard = null;
+        }
+
+        [TestCase(Keys.A)]
+        [TestCase(Keys.W)]
+        [TestCase(Keys.Up)]
+        [TestCase(Keys.Escape)]
+        [TestCase(Keys.S)]
+        [TestCase(Keys.Y)]
+        [TestCase(Keys.Down)]
+        [TestCase(Keys.Tab)]
+        [TestCase(Keys.F1)]
+        [TestCase(Keys.VolumeDown)]
+        [TestCase(Keys.ShiftKey)]
+
+        public void CanRegisterInput_UsualKeys(Keys key)
+        {
+            _keyboardManipulator.Press(key);
+            var isKeyDown = _keyboard.Status.IsKeyDown(key);
+            Assert.IsTrue(isKeyDown);
+        }
+
+        [TestCase(Keys.Alt)]
+        [TestCase(Keys.Shift)]
+        [TestCase(Keys.Control)]
+        public void CanRegisterInput_ModifierKeys(Keys key)
+        {
+            _keyboardManipulator.Press(key);
+            var isKeyDown = _keyboard.Status.IsKeyDown(key);
+            Assert.IsTrue(isKeyDown);
+        }
+
+        [TestCase(Keys.A)]
+        [TestCase(Keys.B)]
+        [TestCase(Keys.ControlKey)]
+        public void CanRegisterJustPressedKeys(Keys key)
+        {
+            _keyboardManipulator.Press(key);
+            var isKeyJustPressed = _keyboard.Status.IsKeyDown(key);
+            Assert.IsTrue(isKeyJustPressed);
+        }
+    }
+}
