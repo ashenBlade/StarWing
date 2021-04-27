a089df76665be423b7a2015038815566c080f4e3
Azaliya Khismatullina <newtmas1956@mal.ru>
2021-04-27 15:16:35 +0300

    [add] StarWing/UI/

---
+++ StarWing/UI/UILabel.cs
@@ -0,0 +1,11 @@
+﻿namespace StarWing.UI
+{
+    public class UILabel: UIComponent
+    {
+        public string Text { get; set; }
+        
+        public float Size { get; set; }
+        
+        public Font Font { get; set; }
+    }
+}
