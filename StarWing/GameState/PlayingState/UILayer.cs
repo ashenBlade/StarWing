a22dec30c68ee289d6d2b3f639726297608fe4a4
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-05-02 16:45:16 +0300

    UILayer contains UI components

---
+++ StarWing/GameState/PlayingState/UILayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using StarWing.ECS;
+using StarWing.Framework;
+using StarWing.UI;
+
+namespace StarWing.GameState
+{
+    public class UILayer
+    {
+        private List<UIComponent> _components;
+
+        public UILayer(IEnumerable<UIComponent> components = null)
+        {
+            _components = new List<UIComponent>();
+            if (components != null)
+            {
+                _components.AddRange(components);
+            }
+        }
+
+        public virtual void Update(GameTime gameTime, Input input)
+        {
+            _components.ForEach(component => component.Update(gameTime, input));
+        }
+
+        public virtual void Render(Graphics graphics)
+        {
+            _components.ForEach(component => component.Render(graphics));
+        }
+    }
+}
