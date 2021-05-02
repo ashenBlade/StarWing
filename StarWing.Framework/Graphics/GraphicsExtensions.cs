79dcd84d5c347a0d5c2cc6148d1c3ffb29eeb246
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-05-02 16:44:13 +0300

    Extension for GDI+ sprite drawing

---
+++ StarWing.Framework/Graphics/GraphicsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace StarWing.Framework
+{
+    public static class GraphicsExtensions
+    {
+        public static void DrawSprite(this Graphics graphics, Sprite sprite, Vector2D position)
+        {
+            if (sprite.Image == null)
+            {
+                throw new ArgumentNullException(nameof(sprite));
+            }
+            graphics.DrawImage(sprite.Image, new Rectangle(position, sprite.Size));
+        }
+    }
+}
