d7f4608160f5e57b15d7de6f81fa75adc3957a2c
Sergey Solovyev <htcdnsnnkom2002@gmail.com>
2021-05-08 13:24:38 +0300

    Add UIBar that represents current/max values as rectangle

---
+++ StarWing/UI/UIBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace StarWing.UI
+{
+    public class UIBar : UIComponent
+    {
+        public float MaxValue { get; set; }
+        public float CurrentValue { get; set; }
+        public Color BarColor { get; set; }
+
+        public UIBar(float maxValue, float startValue) : this(maxValue, startValue, Color.White) { }
+        public UIBar(float maxValue, float startValue, Color barColor)
+        {
+            MaxValue = maxValue;
+            CurrentValue = startValue;
+            BarColor = barColor;
+        }
+    }
+}
