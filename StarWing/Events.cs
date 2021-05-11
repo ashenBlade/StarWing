fa9373cf1c18d7aabb99d6e0c1f891b25e47d8ed
RizvaNM <rizvanmel1999@gmail.com>
2021-05-11 09:59:41 +0300

    Add GameScore (but no image), fix EventManager

---
+++ StarWing/Events.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWing
+{
+    enum eEvents
+    {
+        StartGame, // = StartLevel
+        FinishGame, // This means that Player successfully complete the Quest
+        //PausedGame, 
+        //ResumedGame,
+        DamageEnemy,
+        DamagedPlayer,
+        KillPlayer // Start level again or open UserMenu
+    }
+
+    class Events
+    {
+        public void StartGame()
+        {
+
+        }
+
+        public void FinishGame() { }
+
+        public void DamageEnemy() { }
+
+        public void KillEnemy()
+        { }
+
+        public void DamagedPlayer() { }
+
+        public void KillPlayer() { }
+    }
+}
---
+++ StarWing/GameScore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWing
+{
+    class AddGameScore
+    {
+        private int _currentScore;
+        private int _addScore;
+
+        public AddGameScore(int score, int addScore)
+        {
+            _currentScore = score;
+            _addScore = addScore;
+        }
+
+        public int Score
+        {
+            get
+            {
+                return _currentScore;
+            }
+        }
+
+        public int AddScore
+        {
+            get
+            {
+                return _addScore;
+            }
+        }
+    }
+}
